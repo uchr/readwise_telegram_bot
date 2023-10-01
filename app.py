81a383ad447b5ec15f53a5461718eb00d3c2e422
Anton Tarasov <uchiro.me@gmail.com>
2023-10-02 02:17:27 +0300

    Removed support for Readwise highlights and improve quality of HTML page from telegram message

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ from telegram import *
 from telegram.ext import *
 from readwise import ReadWise
 from datetime import datetime
-import logging
+import markdown
+import markdownify
 from functools import wraps
 from dotenv import load_dotenv
 
@@ -14,7 +15,7 @@ BOT_TOKEN = os.getenv('BOT_TOKEN')
 # initialize class for Readwise api
 WISE = ReadWise(os.getenv('READWISE_TOKEN'))
 # restrict access to our bot to avoid spam
-ADMIN = os.getenv('ADMIN_USER_ID')
+ADMIN = os.getenv('ADMIN_USER')
 
 logging.FileHandler("info_telewise_bot.txt", mode='a', encoding=None, delay=False)
 logging.basicConfig(
@@ -22,83 +23,51 @@ logging.basicConfig(
     level=logging.INFO
 )
 
-FORWARD = range(1)
+SEND_TO_READWISE = range(1)
+
+def get_html(text_html: str) -> str:
+    markdown_from_html = markdownify.markdownify(text_html)
+    return markdown.markdown(markdown_from_html)
 
 def restricted(func):
     @wraps(func)
-    async def wrapped(update, context, *args, **kwargs):
-        user_id = update.effective_user.id
-        if user_id == ADMIN:
-            print(f"Unauthorized access denied for {user_id}.")
+    async def wrapped(update: Update, context, *args, **kwargs):
+        username = update.effective_user.username
+        if username != ADMIN:
+            error_message = f"Unauthorized access denied for {username}."
+            await context.bot.send_message(chat_id=update.effective_chat.id, text=error_message)
+            logging.warning(error_message)
             return
         return await func(update, context, *args, **kwargs)
     return wrapped
     
 
-def url_extracter(entities):
-    for ent in entities:
-        txt = entities[ent]
-        if ent.type == ent.TEXT_LINK:
-            # Text with embedded URL  
-            return str(ent.url)
-        elif ent.type == ent.URL:
-            # Plain URL
-            return str(txt)
-
 @restricted
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="Bot for integration with ReadWise api and Telegram. Forward me posts and I will send them to your ReadWise. For more go to https://github.com/ixnet/telewise_bot")
-@restricted
-async def send_to_readwise(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    print ("[+][+][+][+][+] Message from " + str(update.effective_user.id))
-    # link for the telegram post
-    telegram_link = "<a href='https://t.me/" + str(update.message.forward_from_chat.username) + "/" + str(update.message.forward_from_message_id) + "'>Telegram Link</a>"
-    
-    #default note text
-    note_txt ="from Telegram bot"
-        
-    # if the message contains only text, it will have text_html property, but if the message contains media the text of the message would be in the caption_html property    
-    text = update.message.text_html if update.message.caption_html is None else update.message.caption_html
-    # applend telegram link to the post
-    text = text + "\n\n" + "<a href='https://t.me/" + str(update.message.forward_from_chat.username) + "/" + str(update.message.forward_from_message_id) + "'>Telegram Link</a>"
-    # getting only one link (first link in the post would be here) from the post itself.
-    post_link = url_extracter(update.message.parse_entities())
-    # getting chat or channel name from the original telegram post
-    from_who=str(update.message.forward_from_chat.username)
-    # check token for Readwise API before sending highlight
-    WISE.check_token()
-    #send post as Readwise highlight
-    WISE.highlight(text=text,
-        title=from_who,
-        source_url=telegram_link,
-        highlight_url=post_link,
-        note=note_txt,
-        highlighted_at=str(datetime.now().isoformat())
-    )
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="Message from %s was highlighted with note: %s" % (from_who, note_txt))
-
-@restricted
-async def prepare_reader(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="Sending data to Readwise Reader...")
-    return FORWARD
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Bot for integration with ReadWise api and Telegram. Forward me posts and I will send them to your ReadWise. For more go to https://github.com/uchr/readwise_telegram_bot")
 
 @restricted
 async def send_to_reader(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=f"Prepare text for Readwise")
+
     # link for the telegram post
     telegram_link = "https://t.me/" + str(update.message.forward_from_chat.username) + "/" + str(update.message.forward_from_message_id)
-   
+
     # if the message contains only text, it will have text_html property, but if the message contains media the text of the message would be in the caption_html property    
-    text = update.message.text_html if update.message.caption_html is None else update.message.caption_html
-   
+    text = get_html(update.message.text_html_urled) if update.message.caption_html_urled is None else get_html(update.message.caption_html_urled)
+
+    title = str(update.message.forward_from_chat.username) + " " + str(datetime.now().isoformat())
+
     WISE.check_token()
     #send post as Readwise highlight
     WISE.save(url=telegram_link,
         html=text,
-        title = str(update.message.forward_from_chat.username) + " " + str(datetime.now().isoformat()),
+        title = title,
         summary = text[:128])
     
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="Working with Reader API...")
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=f"{title} was sent to Readwise")
     return ConversationHandler.END
+
 @restricted
 async def cancel(update: Update, context: CallbackContext):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="Oops...")
@@ -107,20 +76,10 @@ async def cancel(update: Update, context: CallbackContext):
 if __name__ == '__main__':
     #start app
     application = ApplicationBuilder().token(BOT_TOKEN).build()
+
     #register commands
-
-    conv_handler_reader = ConversationHandler(
-        entry_points=[MessageHandler(filters.Regex("^r$"), prepare_reader)],
-        states={
-            FORWARD: [MessageHandler((filters.TEXT | filters.ATTACHMENT | filters.PHOTO) & ~filters.COMMAND, send_to_reader)],
-        },
-        fallbacks=[CommandHandler("cancel", cancel)],
-    )
-
-    
-    application.add_handler(conv_handler_reader)
     application.add_handler(CommandHandler('start', start))
-    application.add_handler(MessageHandler((filters.TEXT | filters.ATTACHMENT | filters.PHOTO) & ~filters.COMMAND, send_to_readwise))
+    application.add_handler(MessageHandler((filters.TEXT | filters.ATTACHMENT | filters.PHOTO) & ~filters.COMMAND & filters.FORWARDED, send_to_reader))
     
     #run bot
     application.run_polling()
--- readwise.py
+++ readwise.py
@@ -10,23 +10,6 @@ class ReadWise:
         if r.status_code != 204:
             sys.exit("[+] Readwise token is outdated. Cannot continue working.")
 
-    def highlight(self, **kwargs):
-        # if smth is empty in the telegram post send empty string to Readwise 
-        for v in kwargs.values():
-            if v is None:
-                v = ""
-        r = requests.post(url="https://readwise.io/api/v2/highlights/",headers={"Authorization": "Token %s" % self.token},
-        json={"highlights": [
-                        {
-                        "text": kwargs.get("text"),
-                        "title": kwargs.get("title"),
-                        "source_url": kwargs.get("source_url"),
-                        "source_type": "telewise_bot",
-                        "note": kwargs.get("note"),
-                        "highlighted_at": kwargs.get("highlighted_at")
-                        }
-        ]}
-        )
 
     def save(self, **kwargs):
         # if smth is empty in the telegram post send empty string to Readwise 
